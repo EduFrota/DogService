37dc8b6b214ca2a718dec7822a294fc91fe20e4c
EduFrota <edu-frota@hotmail.com>
2017-05-09 09:51:48 -0300

    Change-Id: I12834b5e37b592aa6dcb7016814af56e6f53b1ea

---
+++ Pet/src/Test/DogTest.java
@@ -0,0 +1,49 @@
+package Test;
+
+import java.util.List;
+
+import br.com.pettinder.domain.Dog;
+import br.com.pettinder.domain.DogService;
+import junit.framework.TestCase;
+
+public class DogTest  extends TestCase{
+   private DogService dogService = new DogService();
+
+   public void testaListaDog(){
+	   List<Dog> dogs = dogService.getDog();
+	   assertNotNull(dogs);
+	   assertTrue(dogs.size()>0);
+	   Dog lucy = dogService.findByName("Lucy").get(0);
+	   assertEquals("Lucy", lucy.getNome());
+   }
+   public void testCrud() {
+		Dog d = new Dog();
+		d.setNome("Nome");
+		d.setIdade("10");
+		d.setRaca("Vira");
+		d.setTemperamento("Agressivo");
+		dogService.save(d);
+		
+		Long id = d.getId();
+		assertNotNull(id);
+		d = dogService.getDog(id);
+		
+		assertEquals("Nome", d.getNome());
+		assertEquals("10", d.getIdade());
+		assertEquals("Vira", d.getRaca());
+		assertEquals("Agressivo", d.getTemperamento());
+		
+		d.setNome("Update");
+		dogService.save(d);
+		d = dogService.getDog(id);
+		assertEquals("Update", d.getNome());
+		dogService.delete(id);
+		d= dogService.getDog(id);
+		assertNull(d);
+		
+	}
+
+
+	
+
+}
